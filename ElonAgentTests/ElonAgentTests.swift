d2a40857ed5226d4fe45a276ab3fdeae8040baf0
yilian <yilian@bytedance.com>
2025-11-19 14:17:13 +0800

    Initial Commit

---
+++ ElonAgentTests/ElonAgentTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElonAgentTests.swift
+//  ElonAgentTests
+//
+//  Created by ByteDance on 11/19/25.
+//
+
+import Testing
+@testable import ElonAgent
+
+struct ElonAgentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
